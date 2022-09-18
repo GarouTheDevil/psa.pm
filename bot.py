7f8c92809dadfe28053987a918a5924566ea44e5
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2022-09-18 08:59:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -146,33 +146,34 @@ async def link_handler(bot, message):
         await mess.edit_text(f"**Error** : {e}")
 
 async def psa_bypass(url):
-client = requests.Session()
-h = {
+    client = requests.Session()
+    h = {
     'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
-}
-res = client.get(url, cookies={}, headers=h)
+    }
 
-url = 'https://try2link.com/'+re.findall('try2link\.com\/(.*?) ', res.text)[0]
+    res = client.get(url, cookies={}, headers=h)
 
-res = client.head(url)
+    url = 'https://try2link.com/'+re.findall('try2link\.com\/(.*?) ', res.text)[0]
 
-id = re.findall('d=(.*?)&', res.headers['location'])[0]
-id = base64.b64decode(id).decode('utf-8')
+    res = client.head(url)
 
-url += f'/?d={id}'
-res = client.get(url)
+    id = re.findall('d=(.*?)&', res.headers['location'])[0]
+    id = base64.b64decode(id).decode('utf-8')
 
-bs4 = BeautifulSoup(res.content, 'html.parser')
-inputs = bs4.find_all('input')
-data = { input.get('name'): input.get('value') for input in inputs }
+    url += f'/?d={id}'
+    res = client.get(url)
 
-time.sleep(6.5)
-res = client.post(
-    'https://try2link.com/links/go',
-    headers={
-        'referer': url,
-        'x-requested-with': 'XMLHttpRequest',
-    }, data=data
+    bs4 = BeautifulSoup(res.content, 'html.parser')
+    inputs = bs4.find_all('input')
+    data = { input.get('name'): input.get('value') for input in inputs }
+
+    time.sleep(6.5)
+    res = client.post(
+        'https://try2link.com/links/go',
+         headers={
+            'referer': url,
+            'x-requested-with': 'XMLHttpRequest',
+          }, data=data
 )
-out = res.json()['url'].replace('\/','/')
+     out = res.json()['url'].replace('\/','/')
