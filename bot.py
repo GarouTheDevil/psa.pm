a9746c1f2c1dac97ab8fbf80787651213b482522
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2022-09-18 13:49:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,11 +19,6 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, Message
 from pyrogram.errors import UserNotParticipant, UserBannedInChannel 
 
-API_ID = environ.get('API_ID')
-API_HASH = environ.get('API_HASH')
-BOT_TOKEN = environ.get('BOT_TOKEN')
-#API_KEY = environ.get('API_KEY')
-
 bot = Client('LinkByPass bot',
              api_id= 1543212,
              api_hash= "d47de4b25ddf79a08127b433de32dc84",
